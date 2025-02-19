0c55f4be10c1ac659cd29a6c4f4a3486b710278b
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-02-19 15:53:21 -0330

    Calculate Sales Tax

--- ChangeMaker/make_change.js
+++ ChangeMaker/make_change.js
@@ -1,2 +1,12 @@
 const $ = selector => document.querySelector(selector);
 
+const processEntry = () => {
+    const amount = parseInt($("amount").value);
+    if(isNaN(amount) || amount< 0 || amount > 99) {
+        alert("Amount must be a valid number between 0 and 99");
+        $("amount").focus();
+    } else {
+        makeChange(amount);
+    }
+}:
+
--- SalesTax/sales_tax.js
+++ SalesTax/sales_tax.js
@@ -2,30 +2,50 @@
 
 const $ = selector => document.querySelector(selector);
 
-const getErrorMsg = lbl => `${lbl} must be a valid number greater than zero less than 10000`;
+const getErrorMsg = lbl => `${lbl} Enter a valid number`
 
 const focusAndSelect = selector => {
     const elem = $(selector);
     elem.focus();
     elem.select();
+}
+
+const clearField = selector => {
+    const elem = $(selector);
+    elem.value = "";
+    focusAndSelect(selector);
 };
+
 const processEntries = () => {
     const subtotal = parseFloat($("#subtotal").value);
     const taxRate = parseFloat($("#tax_rate").value);
     
-    if (isNaN(subtotal) || miles <= 0 || miles >= 10000) {
-        alert(getErrorMsg("Subtotal"));
+    if (isNaN(subtotal) || subtotal <= 0 || subtotal >= 10000) {
+        alert(getErrorMsg("Subtotal must be > 0 and < 10000"));
         focusAndSelect("#subtotal");
     } else if (isNaN(taxRate) || taxRate <= 0 || taxRate >= 12) {
-        alert(getErrorMsg("Tax Rate"));
+        alert(getErrorMsg("Tax rate must be > 0 and < 12"));
         focusAndSelect("#tax_rate");
     } else {
-        $("#sales_tax").value = (subtotal * taxRate / 100).toFixed(2);
+        const salesTax = subtotal * (taxRate / 100);
+        const total = subtotal + salesTax;
+        $("#sales_tax").value = salesTax.toFixed(2);
+        $("#total").value = total.toFixed(2);
     }
+};
+
+const clearAllFields = () => {
+    $("#subtotal").value = "";
+    $("#tax_rate").value = "";
+    $("#sales_tax").value = "";
+    $("#total").focus();
+    focusAndSelect("#subtotal");
 };
 
 document.addEventListener("DOMContentLoaded", () => {
     $("#calculate").addEventListener("click", processEntries);
     $("#subtotal").focus();
+    $("#clear").addEventListener("click", clearAllFields);
+    $("#subtotal").addEventListener("click", () => clearField("#subtotal"));
+    $("#tax_rate").addEventListener("click", () => clearField("#tax_rate"));
 });
-
