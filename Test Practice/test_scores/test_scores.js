e104bc1ba1434160eb95d0ef2b993eb178ecd049
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-02-26 14:24:58 -0330

    Assignment 2 Test Scores project

--- Test Practice/test_scores/test_scores.js
+++ Test Practice/test_scores/test_scores.js
@@ -7,14 +7,17 @@ const score2 = parseInt(prompt("Enter test score"));
 
 const score3 = parseInt(prompt("Enter test score"));
 
-const total = score1 + score2 + score3;
+const score4 = parseInt(prompt("Enter test score"))
+const total = score1 + score2 + score3 + score4;
 
 // calculate the average
-const average = parseInt(total/3);
+const average = parseInt(total/4);
 
 // display in browser page
 const html = `<p>Score 1 = ${score1}</p>
     <p>Score 2 = ${score2}</p>
     <p>Score 3 = ${score3}</p>
+    <p>Score 4 = ${score4}</p>
     <p>Average score = ${average}</p>`;
+
 document.write(html);
