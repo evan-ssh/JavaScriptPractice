dc5fa0eae6fd49eca1bd66015edf6234d7eeb0e4
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-09-12 18:10:19 -0230

    Review assignment Q1

---
+++ ReviewAssignment/2-3 TravelTimeCalculator/tt.js
@@ -0,0 +1,12 @@
+function calcTime(distance,speed){
+    const totalTime = distance / speed
+
+    const hours = Math.floor(totalTime)
+    const minutes = ((totalTime - hours) * 60).toFixed(0)
+    alert(`Distance:${distance}\nSpeed:${speed}MPH\nTravel Time:${hours}hours, ${minutes}minutes`)
+}
+
+
+let distance = parseFloat(prompt("Enter distance travelled in miles"))
+let speed = parseFloat(prompt("Enter average speed in MPH"))
+calcTime(distance,speed)
