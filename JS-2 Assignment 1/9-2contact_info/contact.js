47f416573f753f8ab0a7b659d14c2f9b56fa1928
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-09-26 17:24:11 -0230

    Q2 Manual Validation

--- JS-2 Assignment 1/9-2contact_info/contact.js
+++ JS-2 Assignment 1/9-2contact_info/contact.js
@@ -1,3 +1,80 @@
 "use strict";
 const getElement = selector => document.querySelector(selector);
 
+document.addEventListener("DOMContentLoaded", () => {
+    const nameField = getElement("#name");
+    const emailField = getElement("#email");
+    const phoneField = getElement("#phone");
+    const zipField = getElement("#zip");
+    const dobField = getElement("#dob");
+    const submitBtn = getElement("#save");
+    const resetBtn = getElement("#reset");
+
+    const fields = [nameField,emailField,phoneField,zipField,dobField]
+
+    fields.forEach(field=> {
+        const span = document.createElement("span");
+        span.textContent = "*"
+        field.parentNode.appendChild(span)
+
+        
+    })
+
+
+    submitBtn.addEventListener("click", (evt) =>{
+        let valid = true;
+
+        if(!nameField.value.trim()){
+            valid = false
+            const nameSpan = nameField.parentNode.querySelector("span")
+            nameSpan.textContent = "Please enter your name."
+        }
+
+        if(!emailField.value.trim() && !phoneField.value.trim()){
+            valid = false;
+            const emailSpan = emailField.parentNode.querySelector("span");
+            emailSpan.textContent = "Please enter an email or phone."
+
+        }
+
+        if(!zipField.value.trim() || !/^\d{5}$/.test(zipField.value.trim())){
+            valid = false;
+            const zipSpan = zipField.parentNode.querySelector("span");
+            zipSpan.textContent = "Please enter a 5 digit ZIP."
+        }else{
+            zipField.textContent = "";
+        
+        }
+
+        if(!dobField.value.trim()){
+            valid = false;
+            const dobSpan = dobField.parentNode.querySelector("span");
+            dobSpan.textContent = "Please enter your date of birth.";         
+        }else{
+            const userDate = new Date(dobField.value + "T00:00:00");
+            const currentDate = new Date()
+            currentDate.setHours(0,0,0,0);
+            currentDate.setHours(0,0,0,0);
+            if(userDate > currentDate){
+                valid = false;
+                dobSpan = dobField.parentNode.querySelector("span");
+                dobSpan.textContent = "Please enter a date in the past."
+            }else{
+                dobSpan.textContent = "";
+            }
+            }
+
+        
+
+        if(!valid){
+            evt.preventDefault();
+        }
+    })
+
+    resetBtn.addEventListener("click", () =>{
+        fields.forEach(field =>{
+            field.value = "";
+            field.parentNode.querySelector("span").textContent = "*";
+        })
+    })
+});
