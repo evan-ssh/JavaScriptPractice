29a6a7eae11336b2efcc9a09b6087663cae0d492
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-10-15 17:13:42 -0230

    Contact info

--- JS-2 Assignment 2/contact_info/contact.js
+++ JS-2 Assignment 2/contact_info/contact.js
@@ -3,37 +3,52 @@ const getElement = selector => document.querySelector(selector);
 
 const padNum = num => num.toString().padStart(2, "0");
 
-const clearContact = () => {
-    sessionStorage.removeItem("name");
-    sessionStorage.removeItem("email");
-    sessionStorage.removeItem("phone");
-    sessionStorage.removeItem("zip");
-    sessionStorage.removeItem("dob");
-};
 const saveContact = () => {
-    sessionStorage.name = getElement("#name").value;
-    sessionStorage.email = getElement("#email").value;
-    sessionStorage.phone = getElement("#phone").value;
-    sessionStorage.zip = getElement("#zip").value;
-    sessionStorage.dob = new Date(getElement("#dob").value + "T00:00:00");
+    const contactArr = [
+        getElement("#name").value,
+        getElement("#email").value,
+        getElement("#phone").value,
+        getElement("#zip").value,
+        getElement("#dob").value
+    ];
+    sessionStorage.setItem("contactInfo", JSON.stringify(contactArr));
 };
+
+const clearContact = () => {
+    sessionStorage.removeItem("contactInfo");
+};
+
 const displayContact = () => {
-    getElement("#name").value = sessionStorage.name ?? "";
-    getElement("#email").value = sessionStorage.email ?? "";
-    getElement("#phone").value = sessionStorage.phone ?? "";
-    getElement("#zip").value = sessionStorage.zip ?? "";
-    const dt = new Date(sessionStorage.dob);
-    if(!(dt.toString() == "Invalid Date")) {
-        const str = `${dt.getFullYear()}-${padNum(dt.getMonth() + 1)}-${padNum(dt.getDate())}`;
-        getElement("#dob").value = str;
+    const data = sessionStorage.getItem("contactInfo");
+    if (data) {
+        const contactArr = JSON.parse(data);
+        getElement("#name").value = contactArr[0] ?? "";
+        getElement("#email").value = contactArr[1] ?? "";
+        getElement("#phone").value = contactArr[2] ?? "";
+        getElement("#zip").value = contactArr[3] ?? "";
+     
+        if (contactArr[4]) {
+            const dt = new Date(contactArr[4]);
+            if (!(dt.toString() == "Invalid Date")) {
+                const str = `${dt.getFullYear()}-${padNum(dt.getMonth() + 1)}-${padNum(dt.getDate())}`;
+                getElement("#dob").value = str;
+            }
+        } else {
+            getElement("#dob").value = "";
+        }
     }
 };
+
 const displayConfirmPage = () => {
-    getElement("#lbl_name").textContent = sessionStorage.name ?? "";
-    getElement("#lbl_email").textContent = sessionStorage.email ?? "";
-    getElement("#lbl_phone").textContent = sessionStorage.phone ?? "";
-    getElement("#lbl_zip").textContent = sessionStorage.zip ?? "";
-    getElement("#lbl_dob").textContent = new Date(sessionStorage.dob).toDateString() ?? "";
+    const data = sessionStorage.getItem("contactInfo");
+    if (data) {
+        const contactArr = JSON.parse(data);
+        getElement("#lbl_name").textContent = contactArr[0] ?? "";
+        getElement("#lbl_email").textContent = contactArr[1] ?? "";
+        getElement("#lbl_phone").textContent = contactArr[2] ?? "";
+        getElement("#lbl_zip").textContent = contactArr[3] ?? "";
+        getElement("#lbl_dob").textContent = contactArr[4] ? new Date(contactArr[4]).toDateString(): "";
+    }
 };
 
 const clearMessages = () => {
@@ -42,17 +57,15 @@ const clearMessages = () => {
         const span = input.nextElementSibling;
         if (span) span.textContent = "";
     }
-    inputs[0].focus();
+    if (inputs.length > 0) inputs[0].focus();
 };
 
 document.addEventListener("DOMContentLoaded", () => {
     const form = getElement("form");
 
-    if (form) {  // index.html
-        // turn off default HTML validation messages
+    if (form) { 
         form.noValidate = true;
 
-        // attach invalid event handler for form controls
         for (let element of form.elements) {
             element.addEventListener("invalid", evt => {
                 const elem = evt.currentTarget;
@@ -62,36 +75,31 @@ document.addEventListener("DOMContentLoaded", () => {
             });
         }
 
-        // display data from web storage in contact form
         displayContact();
 
         form.addEventListener("submit", evt => {
-            clearMessages();  
+            clearMessages();
 
-            // validate user has entered an email or a phone number
+
             const email = getElement("#email");
             const phone = getElement("#phone");
-
             let msg = (email.value == "" && phone.value == "") ? "Please enter an email or phone." : "";
             email.setCustomValidity(msg);
 
-            // validate dob 
             const dob = getElement("#dob"); 
-            const dobValue = new Date(dob.value + "T00:00:00");   // add time to correct UTC/local time issue
+            const dobValue = new Date(dob.value + "T00:00:00");
             if (dobValue.toString() == "Invalid Date") {
-                msg = "Please enter a valid DOB."
+                msg = "Please enter a valid DOB.";
             } else {
                 let today = new Date();
-                today = new Date(today.getFullYear(), today.getMonth(), today.getDate()); // sets time to 00:00:00
+                today = new Date(today.getFullYear(), today.getMonth(), today.getDate());
                 msg = (today <= dobValue) ? "DOB must be in the past." : "";
             }
             dob.setCustomValidity(msg);
 
-            // validate form
             if(!form.checkValidity()) { 
                 evt.preventDefault();
             } else {
-                // save contact info to web storage
                 saveContact();
             }
         });
@@ -100,8 +108,7 @@ document.addEventListener("DOMContentLoaded", () => {
             clearMessages();
             clearContact();
         });
-    } else {     // confirm.html
-        // display data from web storage in confirm page labels
+    } else {
         displayConfirmPage();
     }
-}); 
+});
